c6eb71c0d20759d9bff133cbfa0aa0240135c1f2
Philip Müller <philipmuller97@gmail.com>
2024-07-24 02:18:35 +0200

    Built web
    Branch: main
    Commit: 21bb1c191a51d35fba52c9e73082e90ca79370d9
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "57c8ab3653ec0adfc4626fcf4ee2cf92",
+const RESOURCES = {"flutter_bootstrap.js": "5059c2c8b382a2e5a06efd519c033c74",
 "version.json": "51125a046f584beb80c923f44d05395d",
 "index.html": "52c244fe48317e731b4c6f8e387932b4",
 "/": "52c244fe48317e731b4c6f8e387932b4",
-"main.dart.js": "9a32febc8bc7bfa99df5907f373e9ac8",
+"main.dart.js": "c65552d5ed961099594f6443397d8129",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
